337ba958c32a6d17097887f6a774583b560b8d8a
Jacob <jacobgfenner@gmail.com>
2021-01-22 09:10:26 -0500

    quote slideshow

--- lessons.js
+++ lessons.js
@@ -1,26 +1,52 @@
-//FIX BRASS ACADEMY LOGO TO TOP CORNER ON SCROLL
+//LOOP THROUGH STUDENT QUOTES
+const quotes = document.getElementsByClassName('academy-quotes');
 
-const academy = document.getElementById('academy-logo');
-const topOfAcademy = academy.offsetTop;
-let mobile = window.matchMedia('(max-width: 900px)');
+const time = 11000;
+let quoteIndex = 0;
 
-const fixAcademy = () => {
-    if (window.scrollY >= topOfAcademy) {
-        document.body.classList.add('fixed-logo');
-    } else {
-        document.body.classList.remove('fixed-logo');
-    }
-};
+quotes[quoteIndex].classList.toggle('academy-quotes-active');
 
-document.addEventListener('scroll', fixAcademy);
+// setInterval(nextQuote, time);
 
-window.onresize = () => {
-    mobile = window.matchMedia('(max-width: 900px)');
-    if (!mobile.matches) {    
-        document.addEventListener('scroll', fixAcademy);
-    } else {
-        document.removeEventListener('scroll', fixAcademy);
-        document.body.classList.remove('fixed-logo')
-    }
+const nextQuote = () => {
+    quotes[quoteIndex].classList.toggle('academy-quotes-active');
+    quoteIndex += 1;
+    if (quoteIndex >= quotes.length) {quoteIndex = 0};
+    quotes[quoteIndex].classList.toggle('academy-quotes-active');
 }
 
+setInterval(nextQuote, time);
+
+
+//FIX BRASS ACADEMY LOGO TO TOP CORNER ON SCROLL
+
+// const academy = document.querySelector('.academy-logo');
+// const topOfAcademy = academy.offsetTop;
+// let mobile = window.matchMedia('(max-width: 900px)');
+
+// const fixAcademy = () => {
+//     if (window.scrollY >= topOfAcademy) {
+//         document.body.classList.add('fixed-logo');
+//     } else {
+//         document.body.classList.remove('fixed-logo');
+//     }
+// };
+
+// document.addEventListener('scroll', fixAcademy);
+
+// window.onload = () => {
+//     if (mobile.matches) {
+//         document.removeEventListener('scroll', fixAcademy);
+//         document.body.classList.remove('fixed-logo')
+//     }
+// }
+
+// window.onresize = () => {
+//     mobile = window.matchMedia('(max-width: 900px)');
+//     if (!mobile.matches) {    
+//         document.addEventListener('scroll', fixAcademy);
+//     } else {
+//         document.removeEventListener('scroll', fixAcademy);
+//         document.body.classList.remove('fixed-logo')
+//     }
+// }
