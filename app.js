9bc54c440ca97e0fe8feaa78211b78efc2661460
Fenner314 <jacobgfenner@gmail.com>
2021-05-04 12:59:54 -0400

    adjusted slider settings

--- app.js
+++ app.js
@@ -99,6 +99,8 @@ const navbarChange = () => {
 new Glider(document.querySelector('.glider'), {
     slidesToShow: 1,
     draggable: true,
+    scrollLock: true,
+    duration: 1.5,
     dots: '#dots',
     arrows: {
         prev: '.glider-prev',
